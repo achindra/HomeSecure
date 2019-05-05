9175292b449c711a2acf63054b4406d7a209cfa6
Achindra Bhatnagar <achindra@github.com>
2019-05-05 11:20:22 +0530

    HomeSecure

---
+++ homesecure/homesecure.Windows/MainPage.xaml.cs
@@ -0,0 +1,8 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace homesecure
+{
+    public sealed partial class MainPage : Page
+    {
+    }
+}
---
+++ homesecure/homesecure.WindowsPhone/Services/MobileServices/homesecure/push.register.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json.Linq;
+
+// http://go.microsoft.com/fwlink/?LinkId=290986&clcid=0x409
+
+namespace homesecure
+{
+    internal class homesecurePush
+    {
+        public async static void UploadChannel()
+        {
+            var channel = await Windows.Networking.PushNotifications.PushNotificationChannelManager.CreatePushNotificationChannelForApplicationAsync();
+
+            try
+            {
+                await App.homesecureClient.GetPush().RegisterNativeAsync(channel.Uri);    
+                await App.homesecureClient.InvokeApiAsync("notifyAllUsers",
+                    new JObject(new JProperty("toast", "Sample Toast")));
+            }
+            catch (Exception exception)
+            {
+                HandleRegisterException(exception);
+            }
+        }
+
+        private static void HandleRegisterException(Exception exception)
+        {
+
+        }
+    }
+}
